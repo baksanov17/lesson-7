823b6e97548050a665c4f0cf43a8cfa2a140904d
baksanov17 <baksanov17@mail.ru>
2024-08-23 03:41:28 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+# Словарь
+my_dict = {'Vasia':175, 'Vika':160 }
+print(my_dict)
+my_dict['Dima'] = 180
+print(my_dict)
+my_dict.update({'Anton': 165,'Andrey': 174})
+print(my_dict)
+del my_dict['Vasia']
+print(my_dict)
+# Множество
+my_set = {1,2,3,5,5,1,2,9,7,8,9,10}
+my_set.add(411)
+my_set.add(123)
+print(my_set)
+print(my_set.discard(2))
+print(my_set)
+
