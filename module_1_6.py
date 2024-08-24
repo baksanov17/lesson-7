84729e9d411c080389c15f96051598c3c72475b2
baksanov17 <baksanov17@mail.ru>
2024-08-24 09:40:50 +0500

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,17 +1,15 @@
 # Словарь
 my_dict = {'Vasia':175, 'Vika':160 }
 print(my_dict)
-my_dict['Dima'] = 180
-print(my_dict)
+print(my_dict.get('Vasia')), print(my_dict.get('Dima'))
 my_dict.update({'Anton': 165,'Andrey': 174})
 print(my_dict)
-del my_dict['Vasia']
+my_dict.pop('Vasia') # команда del полностью удаляет значение, а рор изымает из словаря, но сохраняет данные
 print(my_dict)
 # Множество
 my_set = {1,2,3,5,5,1,2,9,7,8,9,10}
-my_set.add(411)
-my_set.add(123)
 print(my_set)
+my_set.update({53,49})
 print(my_set.discard(2))
 print(my_set)
 
